9b91f527f399ce8d05ad2f88692c7479c1f20a98
farazleast1 <68387346+farazleast1@users.noreply.github.com>
2020-12-05 14:30:33 +0530

    Update favicon.py

--- favicon.py
+++ favicon.py
@@ -1,5 +1,5 @@
 import requests,mmh3,base64
-response = requests.get('https://website/favicon.ico')
+response = requests.get('https://unomi.apache.org/favicon.ico')
 favicon = base64.encodebytes(response.content)
 hash = mmh3.hash(favicon)
 print(hash)
